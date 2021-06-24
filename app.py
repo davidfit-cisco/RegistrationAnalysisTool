4c0b5cd14ebce476a52fbd1309fc588a0dcc8b3c
David Fitzgerald <davidfit@cisco.com>
2021-06-24 09:29:00 +0100

    Rename to app so heroku can run

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from assets import register_assets
 
 # Entry point to application
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'secret'
-    with app.app_context():
+    reg_state_app = Flask(__name__)
+    reg_state_app.secret_key = 'secret'
+    with reg_state_app.app_context():
         from db.db import init_db
         import routes  # Contrary to PEP, this import here is vital
         init_db()
-    register_assets(app)
-    return app
+    register_assets(reg_state_app)
+    return reg_state_app
 
 
 if __name__ == '__main__':
     # from utils import LoggingMiddleware
     # app.wsgi_app = LoggingMiddleware(app.wsgi_app)
-    reg_state_app = create_app()
-    reg_state_app.run(debug=True)
+    app = create_app()
+    app.run(debug=True)
