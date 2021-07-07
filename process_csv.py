94f5b2bf06d80537e5b5e97374ada59223bb3a0d
David Fitzgerald <davidfit@cisco.com>
2021-07-07 12:22:51 +0100

    Show MAC addresses of the 'Never' SPA's

--- process_csv.py
+++ process_csv.py
@@ -8,6 +8,7 @@ from utils import is_date, add_date_to_data, is_open_dg_store, is_registered, is
 
 
 def process_csv(file_contents):
+    tcp_never_macs = {}
     main_data = {}
     readfile = csv.reader(StringIO(file_contents))
     column_indexes = process_title_row(readfile, main_data)
@@ -20,19 +21,21 @@ def process_csv(file_contents):
             process_row(row, column_indexes, main_data)
 
             if is_tcp_store(tcp_stores, column_indexes, row):
-                process_row(row, column_indexes, tcp_data)
+                process_row(row, column_indexes, tcp_data, tcp_never_macs=tcp_never_macs)
 
-    return render_template('processed.html', main_data=main_data, tcp_data=tcp_data, days=list(main_data.keys()))
+    return render_template('processed.html',
+                           main_data=main_data, tcp_data=tcp_data,
+                           tcp_never_macs=tcp_never_macs, days=list(main_data.keys()))
 
 
-def process_row(row, column_indexes, data):
+def process_row(row, column_indexes, data, tcp_never_macs=None):
     device_type = row[column_indexes["device type"]]
     for day in list(data.keys()):
-        initialise_day_data(day, device_type, data)
-        add_registration_to_data(day, device_type, data, row)
+        initialise_day_data(day, device_type, data, tcp_never_macs=tcp_never_macs)
+        add_registration_to_data(day, device_type, data, row, column_indexes, tcp_never_macs=tcp_never_macs)
 
 
-def add_registration_to_data(day, device_type, data, row):
+def add_registration_to_data(day, device_type, data, row, column_indexes, tcp_never_macs=None):
     reg_count = 0
     unregistered_count = 0
     column_count = len(data[day]["columns"])
@@ -46,14 +49,18 @@ def add_registration_to_data(day, device_type, data, row):
         data[day]['Always'][device_type] += 1
     elif unregistered_count == column_count:
         data[day]['Never'][device_type] += 1
+        if tcp_never_macs is not None and "SPA" in device_type.upper():
+            tcp_never_macs[day].append(row[column_indexes["mac"]])
     else:
         data[day]['Sometimes'][device_type] += 1
 
 
-def initialise_day_data(day, device_type, data):
+def initialise_day_data(day, device_type, data, tcp_never_macs=None):
     if device_type not in data[day]['Always']:
         for option in ('Always', 'Sometimes', 'Never'):
             data[day][option][device_type] = 0
+    if tcp_never_macs is not None and day not in tcp_never_macs:
+        tcp_never_macs[day] = []
 
 
 def process_title_row(readfile, data):
@@ -73,11 +80,11 @@ def get_closed_stores(closed_stores_filename="Closed_DG_Stores_27_May.xlsx"):
     return [row[0].value for row in closed_sheet.rows if row[0].value]
 
 
-def get_tcp_stores(closed_stores_filename="Closed_DG_Stores_27_May.xlsx"):
+def get_tcp_stores(closed_stores_filename="TCP Conversion List 063021.xlsx"):
     wb = openpyxl.load_workbook(os.path.join("docs", closed_stores_filename))
-    closed_sheet = wb["tcp shops"]
-    return [row[0].value for row in closed_sheet.rows if row[0].value]
+    tcp_sheet = wb["TCP Conversion List 063021"]
+    return [row[0].value for row in tcp_sheet.rows if row[0].value]
+
 
 # Constraints
-
-# First row of csv file must be column titles
+# First row of csv file must be column names
